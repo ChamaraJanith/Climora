489773758b694271240fefa7df60c90d55f1da58
GayathDahanayaka <gayathsanvidu@gmail.com>
2026-03-29 13:02:32 +0530

    add ArticlesPage component and update routing for articles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import ProtectedRoute from './components/routing/ProtectedRoute';
 
 import ContentDashboard from './pages/ContentDashboard';
 import UserDashboard from './pages/UserDashboard';
-//import ArticlesPage from './pages/articles/ArticlesPage';
+import ArticlesPage from './pages/articles/ArticlesPage';
 import ArticleDetailPage from './pages/articles/ArticleDetailPage';
 import './App.css';
 
@@ -59,6 +59,8 @@ export default function App() {
       <Route path="/unauthorized" element={<UnauthorizedPage />} />
       <Route path="/articles/:id" element={<ArticleDetailPage />} />
       <Route path="/learn/:id" element={<ArticleDetailPage />} />
+      <Route path="/articles" element={<ArticlesPage />} />
+      <Route path="/learn" element={<ArticlesPage />} />
 
       {/* Legacy redirect */}
       <Route path="/admin-dashboard" element={<Navigate to="/admin/dashboard" replace />} />
